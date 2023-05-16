96aa27db8c67b73d29a2de85d18b4aeaa1ca5674
Baku Hashimoto <mail@baku89.com>
2023-05-17 00:54:40 +0900

    Update demo

--- demo/main.ts
+++ demo/main.ts
@@ -1,3 +1,5 @@
+import saferEval from 'safer-eval'
+
 import Bndr from '../src'
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement
@@ -5,17 +7,57 @@ const ctx = canvas.getContext('2d')
 
 if (!ctx) throw new Error()
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-Bndr.mergeToVec2(
-	Bndr.midi.controlChange(0, 50).map(v => (v / 127) * canvas.width),
-	Bndr.midi.controlChange(0, 51).map(v => (v / 127) * canvas.height)
-).on(([x, y]) => ctx.fillRect(x, y, 10, 10))
-
-Bndr.pointer.down().on(() => ctx.clearRect(0, 0, canvas.width, canvas.height))
-
-window.addEventListener('resize', () => {
+// Update canvas size on window resizes
+function updateCanvasSize() {
 	canvas.width = window.innerWidth
 	canvas.height = window.innerHeight
+}
+window.addEventListener('resize', updateCanvasSize)
+updateCanvasSize()
+
+const code =
+	localStorage.getItem('code') ??
+	`Bndr.pointer.position()
+	.lerp(.2)
+	.on(([x, y]) => {
+		ctx.beginPath()
+		ctx.arc(x, y, 10, 0, Math.PI * 2)
+		ctx.fill()
+	})
+	
+Bndr.pointer.down()
+	.on(() => ctx.clearRect(0, 0, canvas.width, canvas.height))
+`.trim()
+
+// Setup Ace Editor
+const editor = (window as any).ace.edit('editor')
+const session = editor.getSession()
+editor.setTheme('ace/theme/tomorrow')
+editor.renderer.setShowGutter(false)
+session.setOptions({
+	mode: 'ace/mode/javascript',
+	tabSize: 2,
 })
+editor.setHighlightActiveLine(false)
+session.setValue(code)
+editor.container.style.background = 'transparent'
+
+// Re-evaluate the code on change
+editor.on('change', () => {
+	const code = editor.getValue()
+	localStorage.setItem('code', code)
+
+	Bndr.removeAllListeners()
+	setTimeout(() => runCode(code), 1)
+})
+
+runCode(code)
+
+function runCode(code: string) {
+	const context = {
+		Bndr,
+		ctx,
+		canvas,
+	}
+	saferEval(`(() => {${code}})()`, context)
+}
