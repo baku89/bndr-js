40fa380a518cc21a4f832d749e006e9d8c44f90c
Baku Hashimoto <mail@baku89.com>
2023-09-19 03:28:27 +0900

    Update example

--- demo/examples.ts
+++ demo/examples.ts
@@ -156,7 +156,7 @@ Bndr.tuple(
 		`// Adobe Illustrator-like viewport navigation
 //
 // Pan: Space + Drag / Scroll
-// Zoom: Z + Horizontal Drag / Alt + Scroll
+// Zoom: Z + Horizontal Drag / Alt + Scroll / Pinch
 
 const position = Bndr.pointer().position();
 const leftPressed = Bndr.pointer().left.pressed();
@@ -172,13 +172,6 @@ function draw() {
 
 draw();
 
-const center = position.stash(
-	Bndr.combine(
-		leftPressed.down(),
-		Bndr.pointer().scroll({ preventDefault: true })
-	)
-);
-
 const pan = position
 	.while(
 		Bndr.or(
@@ -192,22 +185,32 @@ const pan = position
 		draw();
 	});
 
-const zoom = Bndr.combine(
-	Bndr.pointer()
-		.scroll()
-		.map(([, y]) => y),
-	position
-		.while(Bndr.cascade(Bndr.keyboard.key("z"), leftPressed))
-		.delta((prev, curt) => vec2.sub(curt, prev))
-		.map(([x]) => -x)
-).on((delta) => {
+const zoomByScroll = Bndr.pointer()
+	.scroll()
+	.map(([, y]) => y);
+
+const zoomByDrag = position
+	.while(Bndr.cascade(Bndr.keyboard.key("z"), leftPressed))
+	.delta((prev, curt) => vec2.sub(curt, prev))
+	.map(([x]) => -x);
+
+const zoomByPinch = Bndr.pointer()
+	.pinch()
+	.map((x) => x * 2);
+
+const zoomCenter = position.stash(
+	leftPressed.down(),
+	Bndr.pointer().scroll({ preventDefault: true }),
+	zoomByPinch.constant(true)
+);
+
+const zoom = Bndr.combine(zoomByScroll, zoomByDrag, zoomByPinch).on((delta) => {
 	const scale = mat2d.pivot(
 		mat2d.fromScaling(vec2.of(1.003 ** -delta)),
-		center.value
+		zoomCenter.value
 	);
 	xform = mat2d.multiply(scale, xform);
 	draw();
-});
-`.trim(),
+});`.trim(),
 	],
 ])
