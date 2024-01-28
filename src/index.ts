164a559cc9afd8f6121756e22119f954821c531e
Baku Hashimoto <mail@baku89.com>
2024-01-28 22:56:25 +0900

    Organize exports

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,4 @@
-import * as combinator from './combinator'
-import * as Emitter from './Emitter'
-import * as gamepad from './generator/gamepad'
-import * as keyboard from './generator/keyboard'
-import * as midi from './generator/midi'
-import * as pointer from './generator/pointer'
 import {createScope, disposeAllEmitters, GeneratorPath} from './global'
-
-/**
- * @private
- */
-const Bndr = {
-	...Emitter,
-	...combinator,
-	...gamepad,
-	...keyboard,
-	...midi,
-	...pointer,
-	disposeAllEmitters,
-	createScope,
-}
-
-export {Bndr}
 
 export * from './combinator'
 export * from './Emitter'
@@ -28,4 +6,5 @@ export * from './generator/gamepad'
 export * from './generator/keyboard'
 export * from './generator/midi'
 export * from './generator/pointer'
+export type * from './types'
 export {createScope, disposeAllEmitters, type GeneratorPath}
