4f9110c77fa4495867b2713e9168db4e9884f094
Baku Hashimoto <mail@baku89.com>
2024-06-13 20:48:18 +0900

    Fix styles

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -34,7 +34,7 @@ export default defineUserConfig({
 				href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap',
 			},
 		],
-		['link', {rel: 'icon', href: '/logo.svg'}],
+		['link', {rel: 'icon', href: './logo.svg'}],
 		[
 			'link',
 			{
